d8d7a9a7031261cebf7f44bc393c5e2265eaa33b
Ira Mertsalova <irynamertsalova@gmail.com>
2020-06-21 22:05:27 +0300

    homework js

--- recipe_homework/js/script.js
+++ recipe_homework/js/script.js
@@ -18,20 +18,19 @@ function CreateRecipe() {
         decription: document.querySelector("textarea").value
     };
     if (newRecipe.type == "Dessert") {
-        recipe[0].children[0].src = "../img/dessert.jpg";
+        recipe[0].children[0].src = "https://media-cdn.tripadvisor.com/media/photo-s/08/9e/d5/98/perkins-restaurant-bakery.jpg";
     }
     else if (newRecipe.type == "FirstDish") {
-        recipe[0].children[0].src = "../img/first-dish.jpg";
+        recipe[0].children[0].src = "https://img3.goodfon.ru/wallpaper/big/1/2c/pervoe-blyudo-sup-pyure-gribnoy.jpg";
     }
     else if (newRecipe.type == "SecondDish") {
-        recipe[0].children[0].src = "../img/second-dish.jpg";
+        recipe[0].children[0].src = "https://lh3.googleusercontent.com/proxy/zcwTAqY5laycMZWjfPMu7vtEa3R7abegolsMtzc5JdoXpAWO_SG68iHPEJtv3dXMyuAbHouXEpMlJyGrzvnSW8U6CghhRi32tDr9O0DtjQGUR_j3HRabB-rVpycs1smN8WDM4HNGsNx6dOimNmtg2Vfnkl1g";
     }
     else if (newRecipe.type == "Salat") {
-        recipe[0].children[0].src = "../img/salad.jpg";
+        recipe[0].children[0].src = "https://2.bp.blogspot.com/-kae1zuGM31I/XJ9itQPr6cI/AAAAAAAACH0/Ylj3atNBx4APqAz38Dz7lrPKGi_kXix1gCEwYBhgL/s1600/Screenshot_106.png";
     }
 
     recipe[0].children[2].innerText = newRecipe.name;
-    recipe[0].children[3].innerText = newRecipe.decription;
     document.getElementById("cookingTime").innerHTML += " " + newRecipe.time;
     document.getElementById("difficulty").innerHTML += " " + newRecipe.complexity;
     recipe[0].children[3].innerText = newRecipe.decription;
