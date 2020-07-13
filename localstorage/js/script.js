b7c10da3d9b7b93b267853324444b08086f20acb
Ira Mertsalova <irynamertsalova@gmail.com>
2020-07-13 13:20:50 +0300

    homework js

--- localstorage/js/script.js
+++ localstorage/js/script.js
@@ -1,13 +1,6 @@
 var ID = 1;
 var data = [];
 var users = [];
-// window.onload = () => {
-//     if (localStorage.length != 0) {
-//         users = localStorage.getItem("users");
-//         JSON.parse(localStorage.getItem("users"));
-//         // localStorage.setItem('users', JSON.stringify(users));
-//     }
-// }
 
 let blocks;
 
@@ -20,12 +13,12 @@ let btn = document.getElementById("btn").onclick = function (event) {
         event.preventDefault();
         blocks[i].classList.toggle("block1");
         blocks[i++].classList.toggle("block1");
-
+        console.log(data);
     }
-    // this.style.display = 'none';
 }
 
 save.addEventListener('click', Register);
+console.log(data);
 confirmn.addEventListener('click', SignIn);
 
 function Register(event) {
@@ -50,66 +43,15 @@ function Register(event) {
     localStorage.setItem("users", JSON.stringify(users));
     data = JSON.parse(localStorage.getItem('users'));
     alert("It's okay!");
+    if (event.target.value == 'SignIn') {
+        location.href = './mainWindow.html';
+    }
+    else {
+        return;
+    }
 }
-
-// function SearchUser(login, password) {
-//     for (let i = 0; i < users.length; i++) {
-//         if (users[i].login == login && users[i].password == password) {
-//             return true;
-//         }
-//         return false;
-//     }
-// }
-
 function SignIn(event) {
-    console.log("click");
-    data.forEach(item => {
-        // if (item.login == Name && item.password == Password) {
-            location.href = "../mainWindow.html";
-            event.preventDefault();
-        // alert(item.login);
-        // }
-        // else {
-        //     alert("Fault");
-        // }
-    });
-    // for (let i = 0; i < users.length; i++) {
-    //     if (SearchUser(users[i].login, users[i].password)) {
-    //         event.preventDefault();
-    //     }
-    //     else {
-    //         alert("Not yhis user in localStorage");
-    //     }
-    // }
+    event.preventDefault()
+    location.href = "./mainWindow.html";
+
 }
-
-// blocks = document.getElementsByClassName("block1");
-// for (let i = 0; i < blocks.length; i++) {
-//     event.preventDefault();
-//     blocks[i].classList.add("block1");
-//     blocks[i++].classList.add("block1");
-// }
-// btn.style.display = 'block';
-
-
-// window.onload = () => {
-    //     let data = localStorage.getItem("users");
-    //     if (data != null) {
-        //         ID = data.length;
-        //         users = JSON.parse(data);
-        //     }
-        // }
-        // for (let i = 0; i < localStorage.users.length; i++) {
-            //     if (localStorage.users[i].login == Login) {
-                //         users.push(user);
-                //     }
-                //     else {
-                    //         localStorage.setItem("users", JSON.stringify(users));
-                    //         event.preventDefault();
-                    //     }
-                    // }
-
-
-                    // // else if (SearchUser(Login, Password) == true) {
-
-
--- localstorage/js/script2.js
+++ localstorage/js/script2.js
@@ -1,7 +1,19 @@
-window.onload = () => {
-    if (data != null) {
-        // ID = data.length;
-        localStorage.setItem("users", JSON.stringify(data));
-        // users = JSON.parse(data);
+let users = [];
+let header = document.querySelector('header');
+window.onload = function () {
+    users = JSON.parse(localStorage.getItem('users'));
+    console.log(users);
+    let avatar = document.createElement('div');
+    for (let i = 0; i < users.length; i++) {
+        avatar.innerText = users[i].name.charAt(0);
+        document.querySelector('h2').innerText += ' ' + users[i].name + ' !';
+        document.querySelector('#name').innerText = users[i].name;
+        document.querySelector('#login').innerText = users[i].login;
+        document.querySelector('#email').innerText = users[i].email;
+        document.querySelector('#password').innerText = users[i].password;
+        header.append(avatar);
+        avatar.addEventListener('click', function () {
+            document.querySelector('.info').classList.toggle('display');
+        });
     }
 }
